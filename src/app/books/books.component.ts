96488f63519b3047ad957a10c7b5cb5e232fcf67
CosmicTiger <luisangelmarcia@gmail.com>
2021-03-10 20:42:37 -0600

    :sparkles: - @CosmicTiger - Done Subject and Services implementations in Angular

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BooksService } from '../services/books.service';
+import { Subscription } from 'rxjs';
 
 @Component ({
   selector: 'app-books',
@@ -7,19 +8,31 @@ import { BooksService } from '../services/books.service';
 })
 
 
-export class BooksComponent {
+export class BooksComponent implements OnInit, OnDestroy {
 
   books = [];
 
-  constructor(private booksService: BooksService) {
-    this.books = booksService.getBooks();
-  }
+  constructor(private booksService: BooksService) { }
+
+  private bookSubscription: Subscription;
 
   deleteBook(book) {
   }
 
   saveBook(f) {
     if (f.valid) {
+      this.booksService.addBooks(f.value.bookName);
     }
+  }
+
+  ngOnInit() {
+    this.books = this.booksService.getBooks();
+    this.bookSubscription = this.booksService.booksSubject.subscribe(() => {
+      this.books = this.booksService.getBooks();
+    })
+  }
+
+  ngOnDestroy() {
+    this.bookSubscription.unsubscribe();
   }
 };
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,8 +1,14 @@
+import { Subject } from 'rxjs';
+
 export class BooksService {
+
+  booksSubject = new Subject();
+
   private books = ["Vaxi's Books", "Arithmetic Book", "The Graphic Magazine"];
 
   addBooks(bookName: string) {
     this.books.push(bookName);
+    this.booksSubject.next();
   }
 
   getBooks() {
